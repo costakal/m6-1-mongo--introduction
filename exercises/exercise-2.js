5f7589dd3217a6f2456be2b37c8027ccb7e9789e
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-08-17 13:31:13 -0400

    Exercise 2.1

---
+++ exercises/exercise-2.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require("mongodb");
+const assert = require("assert");
+
+require("dotenv").config();
+
+const { MONGO_URI2 } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const createGreeting = async (req, res) => {
+  try {
+    const client = await MongoClient(MONGO_URI2, options);
+    await client.connect();
+
+    const db = client.db("exercises");
+    console.log("connected!");
+
+    const r = await db.collection("greetings").insertOne(req.body);
+    assert.equal(1, r.insertedCount);
+
+    res.status(201).json({ status: 201, data: req.body });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message });
+  }
+
+  client.close();
+  console.log("disconnected!");
+};
+
+module.exports = {
+  createGreeting,
+};
